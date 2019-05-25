1211553d600e2066cda8fce8348fb36168125d06
Bastian Hodapp <bastianhodapp@gmail.com>
2019-05-25 16:24:09 +0200

    Initial commit

---
+++ All mtg card names/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net;
+
+namespace All_mtg_card_names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Before Request");
+
+            var webRequest = WebRequest.Create(@"https://mtgjson.com/json/AllCards.json");
+
+            var strContent = new StreamReader(webRequest.GetResponse().GetResponseStream()).ReadToEnd();
+
+            JObject jObject = JObject.Parse(strContent);
+
+            Console.WriteLine(jObject);            
+
+            Console.WriteLine("After Request");
+        }
+    }
+}
